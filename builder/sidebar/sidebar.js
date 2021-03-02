1ae6b01f105ba2911d8b4474aadca7653cf39702
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-03-02 20:32:05 +0100

    feat: addes sidebar with clickable button

--- builder/sidebar/sidebar.js
+++ builder/sidebar/sidebar.js
@@ -1,7 +1,9 @@
 import { Box, Spinner } from '@chakra-ui/react'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import { FaPlus, FaRegTimesCircle } from 'react-icons/fa'
 
-const TextBlock = ({ setNewBlockType }) => {
+const TextBlock = ({ setNewBlockType, handleDrop }) => {
 	return (
 		<Box
 			h='50px'
@@ -10,6 +12,7 @@ const TextBlock = ({ setNewBlockType }) => {
 			className='droppable-element'
 			draggable={true}
 			unselectable='on'
+			onDragEnd={handleDrop}
 			onDragStart={(e) => {
 				e.dataTransfer.setData('text/plain', '')
 				setNewBlockType('text')
@@ -37,18 +40,54 @@ const ImageBlock = ({ setNewBlockType }) => {
 }
 
 const BuilderSidebar = ({ setNewBlockType, isSaved }) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	function handleDrop() {
+		setIsOpen(false)
+	}
+	if (!isOpen)
+		return (
+			<Box
+				pos='absolute'
+				top='10px'
+				right='10px'
+				zIndex='9999'
+				border='1px solid transparent'
+				borderRadius='100%'
+				w='60px'
+				h='60px'
+				bg='white'
+				justifyContent='center'
+				d='flex'
+				alignItems='center'
+				cursor='pointer'
+				boxShadow='rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;'
+				_hover={{
+					bg: 'primary.100',
+					border: '1px solid',
+					borderColor: 'primary.500'
+				}}
+				onClick={() => setIsOpen(true)}>
+				<FaPlus size='2.5em' />
+			</Box>
+		)
 	return (
 		<Box
 			d='flex'
 			flexDir='column'
 			pos='fixed'
-			left='0'
+			top='10px'
+			right='10px'
 			zIndex='9999'
 			bg='white'
 			p='10px'
+			boxShadow='rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;'
 			borderRadius='10px'>
+			<Box onClick={() => setIsOpen(false)}>
+				<FaRegTimesCircle size='1em' />
+			</Box>
 			<div>{isSaved ? 'saved' : <Spinner />}</div>
-			<TextBlock setNewBlockType={setNewBlockType} />
+			<TextBlock setNewBlockType={setNewBlockType} handleDrop={handleDrop} />
 			<ImageBlock setNewBlockType={setNewBlockType} />
 		</Box>
 	)
@@ -62,7 +101,8 @@ ImageBlock.propTypes = {
 	setNewBlockType: PropTypes.any
 }
 TextBlock.propTypes = {
-	setNewBlockType: PropTypes.any
+	setNewBlockType: PropTypes.func,
+	handleDrop: PropTypes.func
 }
 
 export default BuilderSidebar
