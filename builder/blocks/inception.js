aea09d7ed0d021579739fd0f184a5de002a36071
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-03-19 21:13:12 +0100

    fix: bug fixes

--- builder/blocks/inception.js
+++ builder/blocks/inception.js
@@ -6,14 +6,14 @@ import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 import { forwardRef, useEffect, useRef, useState } from 'react'
 import {
-	addBlock,
-	editBlock,
-	editItemDraggableProperty,
-	generateBuilderBlocks,
-	addCallbackToBlock,
-	normalizeLayout,
-	normalizeBlockStructure,
-	denormalizeInceptionBlock
+  addBlock,
+  editBlock,
+  editItemDraggableProperty,
+  generateBuilderBlocks,
+  addCallbackToBlock,
+  normalizeLayout,
+  normalizeBlockStructure,
+  denormalizeInceptionBlock,
 } from '../web-builder/helpers'
 import { Box } from '@chakra-ui/react'
 import { GRID_COLUMNS } from '../web-builder/constants'
@@ -21,145 +21,147 @@ import { GRID_COLUMNS } from '../web-builder/constants'
 const ReactGridLayout = WidthProvider(RGL)
 
 function isObjectOutside(newItem, rowHeight, gridRef) {
-	const parentHeight = gridRef.current.parentElement.getBoundingClientRect()
-		.height
-	const rowsNumber = Math.round(parentHeight / rowHeight)
-	return newItem.y + newItem.h > rowsNumber
+  const parentHeight = gridRef.current.parentElement.getBoundingClientRect()
+    .height
+  const rowsNumber = Math.round(parentHeight / rowHeight)
+  return newItem.y + newItem.h > rowsNumber
 }
 
 const BlockInception = forwardRef(({ extraProps, ...data }, ref) => {
-	const {
-		reRender,
-		selectedItemId,
-		newBlockType = 'text',
-		layoutCallback,
-		blockKey: parentBlockKey,
-		setSelectedItem,
-		rowHeight
-	} = extraProps
-	const [newBlockId, setNewBlockId] = useState(() => uuid())
-	const [secondRender, setSecondRender] = useState(false)
-	const gridRef = useRef()
-	const [blocksConfig, udpateBlocksConfig] = useState(() =>
-		normalizeBlockStructure(data.blocks)
-	)
-	const [layout, setLayout] = useState(() => normalizeLayout(data.blocks))
+  const {
+    reRender,
+    selectedItemId,
+    newBlockType = 'text',
+    layoutCallback,
+    blockKey: parentBlockKey,
+    setSelectedItem,
+    rowHeight,
+  } = extraProps
+  const [newBlockId, setNewBlockId] = useState(() => uuid())
+  const [secondRender, setSecondRender] = useState(false)
+  const gridRef = useRef()
+  const [blocksConfig, udpateBlocksConfig] = useState(() =>
+    normalizeBlockStructure(data.blocks)
+  )
+  const [layout, setLayout] = useState(() => normalizeLayout(data.blocks))
 
-	useEffect(() => {
-		udpateBlocksConfig((blocksConfig) =>
-			addCallbackToBlock(blocksConfig, editBlockCallback)
-		)
-	}, [])
+  useEffect(() => {
+    udpateBlocksConfig((blocksConfig) =>
+      addCallbackToBlock(blocksConfig, editBlockCallback)
+    )
+  }, [])
 
-	const editBlockCallback = (newData, blockId, operationType) => {
-		udpateBlocksConfig((blocksConfig) => {
-			const newBlocksConfig = editBlock(
-				blocksConfig,
-				blockId,
-				newData,
-				operationType
-			)
-			layoutCallback(
-				denormalizeInceptionBlock(layout, newBlocksConfig),
-				parentBlockKey
-			)
-			return newBlocksConfig
-		})
-	}
+  const editBlockCallback = (newData, blockId, operationType) => {
+    udpateBlocksConfig((blocksConfig) => {
+      const newBlocksConfig = editBlock(
+        blocksConfig,
+        blockId,
+        newData,
+        operationType
+      )
+      layoutCallback(
+        denormalizeInceptionBlock(layout, newBlocksConfig),
+        parentBlockKey
+      )
+      return newBlocksConfig
+    })
+  }
 
-	useEffect(() => {
-		setLayout((layout) => editItemDraggableProperty(layout, selectedItemId))
-	}, [selectedItemId])
+  useEffect(() => {
+    setLayout((layout) => editItemDraggableProperty(layout, selectedItemId))
+  }, [selectedItemId])
 
-	const updateLayout = (layout) => {
-		if (layout?.length !== Object.keys(blocksConfig)?.length) return
-		setLayout(layout)
-		layoutCallback(
-			denormalizeInceptionBlock(layout, blocksConfig),
-			parentBlockKey
-		)
-	}
+  const updateLayout = (layout) => {
+    if (layout?.length !== Object.keys(blocksConfig)?.length) return
+    setLayout(layout)
+    layoutCallback(
+      denormalizeInceptionBlock(layout, blocksConfig),
+      parentBlockKey
+    )
+  }
 
-	function onDrop(layout, droppedBlockLayout) {
-		if (newBlockType === 'inception') return null
-		setLayout(layout)
-		udpateBlocksConfig((blocksConfig) => {
-			const newBlocksConfig = addBlock(
-				droppedBlockLayout?.i,
-				newBlockType,
-				blocksConfig,
-				editBlockCallback
-			)
-			layoutCallback(
-				denormalizeInceptionBlock(layout, newBlocksConfig),
-				parentBlockKey
-			)
-			return newBlocksConfig
-		})
-		setNewBlockId(uuid())
-	}
+  function onDrop(layout, droppedBlockLayout) {
+    if (newBlockType === 'inception') return null
+    setLayout(layout)
+    udpateBlocksConfig((blocksConfig) => {
+      const newBlocksConfig = addBlock(
+        droppedBlockLayout?.i,
+        newBlockType,
+        blocksConfig,
+        editBlockCallback
+      )
+      layoutCallback(
+        denormalizeInceptionBlock(layout, newBlocksConfig),
+        parentBlockKey
+      )
+      return newBlocksConfig
+    })
+    setNewBlockId(uuid())
+  }
 
-	function handleLayoutChange(layout, _, newItem) {
-		if (isObjectOutside(newItem, rowHeight, gridRef)) {
-			return setSecondRender(uuid())
-		}
-		updateLayout(layout)
-	}
+  function handleLayoutChange(layout, _, newItem) {
+    if (isObjectOutside(newItem, rowHeight, gridRef)) {
+      return setSecondRender(uuid())
+    }
+    updateLayout(layout)
+  }
 
-	useEffect(() => {
-		setSecondRender(uuid())
-	}, [reRender])
+  useEffect(() => {
+    setSecondRender(uuid())
+  }, [reRender])
 
-	const isDroppable = selectedItemId?.includes('inception')
-	const { contentEditable, ...extraData } = data
-	return (
-		<Box
-			{...extraData}
-			w='100%'
-			h='100%'
-			id='inception'
-			cursor='pointer'
-			outline='1px dashed lightgray'>
-			<ReactGridLayout
-				innerRef={gridRef}
-				key={secondRender}
-				cols={GRID_COLUMNS}
-				rowHeight={rowHeight}
-				margin={[0, 0]}
-				style={{ width: '100%', height: '100%' }}
-				autoSize={false}
-				preventCollision={true}
-				isDroppable={isDroppable}
-				onDrop={onDrop}
-				droppingItem={{ i: 'child-inception-' + newBlockId, w: 20, h: 10 }}
-				verticalCompact={false}
-				className='layout'
-				onDragStop={handleLayoutChange}
-				onResizeStop={handleLayoutChange}
-				layout={layout}>
-				{generateBuilderBlocks(
-					blocksConfig,
-					setSelectedItem,
-					layout,
-					selectedItemId
-				)}
-			</ReactGridLayout>
-		</Box>
-	)
+  const isDroppable = selectedItemId?.includes('inception')
+  const { contentEditable, ...extraData } = data
+  return (
+    <Box
+      {...extraData}
+      w="100%"
+      h="100%"
+      id="inception"
+      cursor="pointer"
+      outline="1px dashed lightgray"
+    >
+      <ReactGridLayout
+        innerRef={gridRef}
+        key={secondRender}
+        cols={GRID_COLUMNS}
+        rowHeight={rowHeight}
+        margin={[0, 0]}
+        style={{ width: '100%', height: '100%' }}
+        autoSize={false}
+        preventCollision={true}
+        isDroppable={isDroppable}
+        onDrop={onDrop}
+        droppingItem={{ i: 'child-inception-' + newBlockId, w: 20, h: 10 }}
+        verticalCompact={false}
+        className="layout"
+        onDragStop={handleLayoutChange}
+        onResizeStop={handleLayoutChange}
+        layout={layout}
+      >
+        {generateBuilderBlocks(
+          blocksConfig,
+          setSelectedItem,
+          layout,
+          selectedItemId
+        )}
+      </ReactGridLayout>
+    </Box>
+  )
 })
 
 BlockInception.displayName = 'BlockInception'
 
 BlockInception.propTypes = {
-	extraProps: PropTypes.shape({
-		reRender: PropTypes.bool,
-		blockKey: PropTypes.string,
-		newBlockType: PropTypes.string,
-		selectedItemId: PropTypes.string,
-		layoutCallback: PropTypes.func,
-		setSelectedItem: PropTypes.func,
-		rowHeight: PropTypes.number
-	})
+  extraProps: PropTypes.shape({
+    reRender: PropTypes.bool,
+    blockKey: PropTypes.string,
+    newBlockType: PropTypes.string,
+    selectedItemId: PropTypes.string,
+    layoutCallback: PropTypes.func,
+    setSelectedItem: PropTypes.func,
+    rowHeight: PropTypes.number,
+  }),
 }
 
 export default BlockInception
--- builder/web-builder/web-builder.js
+++ builder/web-builder/web-builder.js
@@ -8,15 +8,15 @@ import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
 import {
-	denormalizeBlockData,
-	generateBuilderBlocks,
-	normalizeBlockStructure,
-	normalizeLayout,
-	saveOnLocal,
-	addBlock,
-	addCallbackToBlock,
-	editBlock,
-	editItemDraggableProperty
+  denormalizeBlockData,
+  generateBuilderBlocks,
+  normalizeBlockStructure,
+  normalizeLayout,
+  saveOnLocal,
+  addBlock,
+  addCallbackToBlock,
+  editBlock,
+  editItemDraggableProperty,
 } from './helpers'
 import { GRID_COLUMNS, ROW_HEIGHT } from './constants'
 import { DELETE } from '../blocks/constants'
@@ -24,145 +24,148 @@ import { DELETE } from '../blocks/constants'
 const ReactGridLayout = WidthProvider(RGL)
 
 const WebBuilder = ({ userBlocksData, newBlockType, setIsSaved }) => {
-	const [newBlockId, setNewBlockId] = useState(() => uuid())
-	const [reRender, setReRender] = useState(false)
-	const [rowHeight, setRowHeight] = useState(ROW_HEIGHT)
-	const [selectedItemId, setSelectedItem] = useState(null)
-	const [realBlockData, setRealBlockData] = useState(userBlocksData)
-	const [blocksConfig, udpateBlocksConfig] = useState(() =>
-		normalizeBlockStructure(realBlockData)
-	)
-	const [layout, updateLayout] = useState(() => normalizeLayout(realBlockData))
+  const [newBlockId, setNewBlockId] = useState(() => uuid())
+  const [reRender, setReRender] = useState(false)
+  const [rowHeight, setRowHeight] = useState(ROW_HEIGHT)
+  const [selectedItemId, setSelectedItem] = useState(null)
+  const [realBlockData, setRealBlockData] = useState(userBlocksData)
+  const [blocksConfig, udpateBlocksConfig] = useState(() =>
+    normalizeBlockStructure(realBlockData)
+  )
+  const [layout, updateLayout] = useState(() => normalizeLayout(realBlockData))
 
-	const debouncedSaved = () => {
-		saveOnLocal(denormalizeBlockData(layout, { ...blocksConfig }), setIsSaved)
-	}
+  const debouncedSaved = () => {
+    saveOnLocal(denormalizeBlockData(layout, { ...blocksConfig }), setIsSaved)
+  }
 
-	useEffect(() => {
-		debouncedSaved(layout, blocksConfig)
-	}, [layout, blocksConfig])
+  useEffect(() => {
+    debouncedSaved(layout, blocksConfig)
+  }, [layout, blocksConfig])
 
-	useEffect(() => {
-		udpateBlocksConfig((blocksConfig) =>
-			addCallbackToBlock(blocksConfig, editBlockCallback)
-		)
-		window.addEventListener('resize', handleWindowResize)
-		window.addEventListener('keydown', handleKeyPress)
-		setRowHeight(window?.innerWidth / GRID_COLUMNS)
-		return () => {
-			window.removeEventListener('keydown', handleKeyPress)
-			window.removeEventListener('resize', handleWindowResize)
-		}
-	}, [])
+  useEffect(() => {
+    udpateBlocksConfig((blocksConfig) =>
+      addCallbackToBlock(blocksConfig, editBlockCallback)
+    )
+    window.addEventListener('resize', handleWindowResize)
+    window.addEventListener('keydown', handleKeyPress)
+    setRowHeight(window?.innerWidth / GRID_COLUMNS)
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress)
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [])
 
-	function setBlockEditable(editableBlockId) {
-		setSelectedItem(editableBlockId)
-		updateLayout((layout) => editItemDraggableProperty(layout, editableBlockId))
-	}
+  function setBlockEditable(editableBlockId) {
+    setSelectedItem(editableBlockId)
+    updateLayout((layout) => editItemDraggableProperty(layout, editableBlockId))
+  }
 
-	const editBlockCallback = (newData, blockId, operationType) => {
-		if (operationType === DELETE) setSelectedItem(null)
-		udpateBlocksConfig((blocksConfig) =>
-			editBlock(blocksConfig, blockId, newData, operationType)
-		)
-	}
+  const editBlockCallback = (newData, blockId, operationType) => {
+    if (operationType === DELETE) setSelectedItem(null)
+    udpateBlocksConfig((blocksConfig) =>
+      editBlock(blocksConfig, blockId, newData, operationType)
+    )
+  }
 
-	function onDrop(layout, droppedBlockLayout) {
-		updateLayout(layout)
-		udpateBlocksConfig((blocksConfig) =>
-			addBlock(
-				droppedBlockLayout?.i,
-				newBlockType,
-				blocksConfig,
-				editBlockCallback
-			)
-		)
-		setNewBlockId(uuid())
-	}
+  function onDrop(layout, droppedBlockLayout) {
+    updateLayout(layout)
+    udpateBlocksConfig((blocksConfig) =>
+      addBlock(
+        droppedBlockLayout?.i,
+        newBlockType,
+        blocksConfig,
+        editBlockCallback
+      )
+    )
+    setNewBlockId(uuid())
+  }
 
-	const onLayoutChange = (layout) => {
-		if (layout?.length !== Object.keys(blocksConfig)?.length) return
-		updateLayout(layout)
-	}
+  const onLayoutChange = (layout) => {
+    if (layout?.length !== Object.keys(blocksConfig)?.length) return
+    updateLayout(layout)
+  }
 
-	function handleWindowResize() {
-		setRowHeight(window?.innerWidth / GRID_COLUMNS)
-	}
+  function handleWindowResize() {
+    setRowHeight(window?.innerWidth / GRID_COLUMNS)
+  }
 
-	function handleResize(_, oldItem, newItem) {
-		if (newItem.i.includes('inception') && newItem.w !== oldItem.w) {
-			setReRender((value) => !value)
-		}
-	}
+  function handleResize(_, oldItem, newItem) {
+    if (newItem.i.includes('inception') && newItem.w !== oldItem.w) {
+      setReRender((value) => !value)
+    }
+  }
 
-	function handleKeyPress(e) {
-		if (e.key === 'Escape') {
-			setBlockEditable(null)
-		}
-	}
+  function handleKeyPress(e) {
+    if (e.key === 'Escape') {
+      setBlockEditable(null)
+    }
+  }
 
-	function layoutCallback(newBlocks, parentBlockKey) {
-		const newBlocksConfig = {
-			...blocksConfig,
-			[parentBlockKey]: {
-				...blocksConfig[parentBlockKey],
-				data: {
-					...blocksConfig[parentBlockKey]?.data,
-					...newBlocks
-				}
-			}
-		}
-		udpateBlocksConfig(newBlocksConfig)
-		saveOnLocal(denormalizeBlockData(layout, newBlocksConfig))
-	}
+  function layoutCallback(newBlocks, parentBlockKey) {
+    udpateBlocksConfig((blocksConfig) => {
+      const newBlocksConfig = {
+        ...blocksConfig,
+        [parentBlockKey]: {
+          ...blocksConfig[parentBlockKey],
+          data: {
+            ...blocksConfig[parentBlockKey]?.data,
+            ...newBlocks,
+          },
+        },
+      }
+      saveOnLocal(denormalizeBlockData(layout, newBlocksConfig))
+      return newBlocksConfig
+    })
+  }
 
-	const isDroppable = !selectedItemId?.includes('inception')
-
-	return (
-		<Box
-			d='flex'
-			w='100%'
-			flexDir='row'
-			onClick={() => setBlockEditable(null)}
-			id='main-builder'>
-			<ReactGridLayout
-				cols={GRID_COLUMNS}
-				rowHeight={rowHeight}
-				onDrop={onDrop}
-				margin={[0, 0]}
-				autoSize
-				preventCollision={true}
-				isDroppable={isDroppable}
-				onResizeStop={handleResize}
-				verticalCompact={false}
-				// This makes everything go 6x slower
-				// This also makes the drop block go crazy while dragging
-				useCSSTransforms={isDroppable}
-				droppingItem={{ i: `${newBlockType}-${newBlockId}`, w: 15, h: 10 }}
-				style={{ width: '100%', minHeight: '100vh', height: '100%' }}
-				className={'layout'}
-				layout={layout}
-				onLayoutChange={onLayoutChange}>
-				{generateBuilderBlocks(
-					blocksConfig,
-					setBlockEditable,
-					layout,
-					selectedItemId,
-					reRender,
-					newBlockType,
-					layoutCallback,
-					setSelectedItem,
-					rowHeight
-				)}
-			</ReactGridLayout>
-		</Box>
-	)
+  const isDroppable = !selectedItemId?.includes('inception')
+  return (
+    <Box
+      d="flex"
+      w="100%"
+      flexDir="row"
+      onClick={() => setBlockEditable(null)}
+      id="main-builder"
+    >
+      <ReactGridLayout
+        cols={GRID_COLUMNS}
+        rowHeight={rowHeight}
+        onDrop={onDrop}
+        margin={[0, 0]}
+        autoSize
+        preventCollision={true}
+        isDroppable={isDroppable}
+        onResizeStop={handleResize}
+        verticalCompact={false}
+        // This makes everything go 6x slower
+        // This also makes the drop block go crazy while dragging
+        useCSSTransforms={isDroppable}
+        droppingItem={{ i: `${newBlockType}-${newBlockId}`, w: 15, h: 10 }}
+        style={{ width: '100%', minHeight: '100vh', height: '100%' }}
+        className={'layout'}
+        layout={layout}
+        onLayoutChange={onLayoutChange}
+      >
+        {generateBuilderBlocks(
+          blocksConfig,
+          setBlockEditable,
+          layout,
+          selectedItemId,
+          reRender,
+          newBlockType,
+          layoutCallback,
+          setSelectedItem,
+          rowHeight
+        )}
+      </ReactGridLayout>
+    </Box>
+  )
 }
 
 WebBuilder.propTypes = {
-	userBlocksData: PropTypes.any,
-	newBlockType: PropTypes.any,
-	setIsSaved: PropTypes.any
+  userBlocksData: PropTypes.any,
+  newBlockType: PropTypes.any,
+  setIsSaved: PropTypes.any,
 }
 
 export default WebBuilder
