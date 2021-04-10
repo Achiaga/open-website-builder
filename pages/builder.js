0a34ba36b35f84d037c16d20e249433e6c319a23
Achiaga <alfonso.achiaga@gmail.com>
2021-04-10 12:48:34 +0200

    fix: hotjar builder

--- pages/builder.js
+++ pages/builder.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
-
+import { InitializeAnalytics } from '../utils/analytics'
 import { Builder } from '../builder'
 
 const BuilderPage = () => {
+  useEffect(() => {
+    try {
+      InitializeAnalytics()
+    } catch (err) {
+      console.error(err)
+    }
+  }, [])
   return (
     <UserProvider>
       <Builder />
