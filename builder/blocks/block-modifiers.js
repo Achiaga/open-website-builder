b79b46c0ada187a93b0118985b752b338c8ca806
Achiaga <alfonso.achiaga@gmail.com>
2021-04-10 12:23:54 +0200

    fix: emojis grid height

--- builder/blocks/block-modifiers.js
+++ builder/blocks/block-modifiers.js
@@ -324,7 +324,7 @@ function EmojiDropDownSelector({
         right={isBlockAtRight ? '0px' : 'unset'}
         bg="white"
         rounded="5px"
-        height={isOpen && '5.5rem'}
+        maxHeight="5.5rem"
         overflow="scroll"
         zIndex="999999"
         boxShadow="rgb(15 15 15 / 5%) 0px 0px 0px 1px, rgb(15 15 15 / 10%) 0px 3px 6px, rgb(15 15 15 / 20%) 0px 9px 24px;"
