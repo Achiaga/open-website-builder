f4769208ffc1f6757c52dde374298e4fb11703f7
Achiaga <alfonso.achiaga@gmail.com>
2021-04-11 18:47:38 +0200

    feat: change default image

--- builder/initial-data.js
+++ builder/initial-data.js
@@ -1,2 +1,2 @@
 export const imageURL =
-  'https://images.unsplash.com/photo-1542103749-8ef59b94f47e'
+  'https://images.unsplash.com/photo-1618159072219-620bc5c8f3f0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80'
