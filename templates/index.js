e1629425afba45887818c55876da4ed9ca3ffb3a
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-04-11 15:39:58 +0200

    feat: added template 4

--- templates/index.js
+++ templates/index.js
@@ -1,11 +1,13 @@
+import fallback from './fallback.json'
 import template1 from './template1.json'
 import template2 from './template2.json'
 import template3 from './template3.json'
-import fallback from './fallback.json'
+import template4 from './template4.json'
 
 export default {
   fallback,
   template1,
   template2,
   template3,
+  template4,
 }
