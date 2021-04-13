8b95894bfec2d1d5d3b65644f1682d9b202bec63
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-04-13 07:57:28 +0200

    feat: improved blog page

--- articles/article-1.js
+++ articles/article-1.js
@@ -1,10 +1,17 @@
-import { Box, List, ListItem, Text } from '@chakra-ui/react'
+import { Box, List, ListIcon, ListItem, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import Navbar from '../components/navbar'
-
+import { FiZap } from 'react-icons/fi'
 const BannerImage = ({ height, width, imageUrl }) => {
   return (
-    <Box height={height} w={width} d="flex" pos="relative" overflow="hidden">
+    <Box
+      height={height}
+      w={width}
+      d="flex"
+      pos="relative"
+      overflow="hidden"
+      mt={['2rem', 0]}
+    >
       <Image
         src={imageUrl}
         layout="fill"
@@ -18,7 +25,8 @@ const BannerImage = ({ height, width, imageUrl }) => {
 const Header = ({ children }) => {
   return (
     <Text
-      fontSize="6xl"
+      as="h1"
+      fontSize={['42px', '6xl']}
       color="gray.500"
       fontWeight="700"
       textAlign="center"
@@ -31,10 +39,11 @@ const Header = ({ children }) => {
 const Description = ({ children }) => {
   return (
     <Text
+      as="p"
       fontSize="xl"
       color="gray.500"
       fontWeight="500"
-      textAlign="left"
+      textAlign={['justify', 'left']}
       py="1rem"
     >
       {children}
@@ -44,10 +53,63 @@ const Description = ({ children }) => {
 const ArticleDate = ({ children }) => {
   return (
     <Text
-      fontSize="xl"
+      as="time"
+      fontSize={['md', 'lg']}
       color="gray.400"
-      fontWeight="300"
+      fontWeight="400"
       textAlign="center"
+      justifyContent="center"
+      d="flex"
+      pt={['1rem', '2rem']}
+    >
+      {children}
+    </Text>
+  )
+}
+
+const TLDR = ({ items }) => {
+  return (
+    <Box px={[0, '5rem']} py="2rem">
+      <Text fontSize="2xl" py="1rem" as="h2">
+        TLDR
+      </Text>
+      <List fontSize="xl" spacing={4}>
+        {items.map((item, index) => {
+          return (
+            <ListItem d="flex" key={index} alignItems="center">
+              <ListIcon as={FiZap} color="primary.500" />
+              <Text>{item}</Text>
+            </ListItem>
+          )
+        })}
+      </List>
+    </Box>
+  )
+}
+
+const Parragraph = ({ children }) => {
+  return (
+    <Text
+      as="p"
+      fontSize="xl"
+      color="gray.500"
+      fontWeight="300"
+      textAlign={['justify', 'left']}
+      pt="1rem"
+      pb="1rem"
+    >
+      {children}
+    </Text>
+  )
+}
+const Subtitle = ({ children }) => {
+  return (
+    <Text
+      as="h2"
+      fontSize="2xl"
+      color="gray.500"
+      fontWeight="600"
+      textAlign="left"
       pt="2rem"
     >
       {children}
@@ -55,21 +117,9 @@ const ArticleDate = ({ children }) => {
   )
 }
 
-const TLDR = () => {
-  return (
-    <Text>
-      <List>
-        <ListItem>Good</ListItem>
-        <ListItem>Super Good</ListItem>
-        <ListItem>Extra super good</ListItem>
-      </List>
-    </Text>
-  )
-}
-
 const ContentWrapper = ({ children }) => {
   return (
-    <Box maxW={['80vw', '60vw']} m="auto">
+    <Box maxW={['100%', '60vw']} m="auto">
       {children}
     </Box>
   )
@@ -81,12 +131,12 @@ const Article1 = () => {
       <Box>
         <Navbar />
         <BannerImage
-          height={'40vw'}
+          height={['80vw', '40vw']}
           width={'100vw'}
           redirectUrl="blog/article-1"
           imageUrl="https://images.unsplash.com/photo-1618159072219-620bc5c8f3f0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1525&q=80"
         />
-        <Box px="10rem" zIndex="1">
+        <Box px={['1.5rem', '10rem']} zIndex="1" pb="10rem">
           <ArticleDate>April 10, 2021</ArticleDate>
           <Header>How to make the switch to Pitch</Header>
           <ContentWrapper>
@@ -97,8 +147,26 @@ const Article1 = () => {
               together a guide to help new teams successfully make the
               transition. Here are our top tips.
             </Description>
-            <TLDR></TLDR>
-            <Box>Text 1</Box>
+            <TLDR items={['1', '2', '3']} />
+            <Parragraph>
+              Since January, we’ve shipped 13 new features and 55 improvements
+              that make creating and collaborating on decks fast and easy. Check
+              out the latest updates and see why it’s never been a better time
+              to get your team on board.
+            </Parragraph>
+            <Subtitle>Make the switch to Pitch faster</Subtitle>
+            <Parragraph>
+              When teams have a central space to work they can accomplish
+              anything — from raising money to delivering more customer-centric
+              products. But getting teams to change their ways of working can be
+              tough, even when the benefits outweigh the temporary discomfort of
+              doing things differently.
+            </Parragraph>
+            <Parragraph>
+              So this quarter, we set out to make it easy for teams to get
+              started, migrate over their existing materials, and create
+              beautiful decks quicker than ever.
+            </Parragraph>
           </ContentWrapper>
         </Box>
       </Box>
