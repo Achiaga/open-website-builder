9270b583ef43d533a77c21ea2f1d7e95caf06463
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-04-18 16:54:40 +0200

    feat: new features section and improved mobile layout for video

--- components/video-demo/video-demo.js
+++ components/video-demo/video-demo.js
@@ -1,11 +1,21 @@
-import {Box} from "@chakra-ui/react"
+import { Box } from '@chakra-ui/react'
 
 const VideoDemo = () => {
-    return(
-        <Box borderRadius="10px">
-            <video style={{borderRadius:"10px", boxShadow:"0 6px 12px -2px rgba(50,50,93,0.25),0 3px 7px -3px rgba(0,0,0,0.3)"}} src={"/hero-video.mp4"} width="1100" height="800" controls="controls" />
-        </Box>
-    )
+  return (
+    <Box borderRadius={['5px', '10px']} px={['1rem', 0]} pb={['1rem', 0]}>
+      <video
+        style={{
+          borderRadius: '10px',
+          boxShadow:
+            '0 6px 12px -2px rgba(50,50,93,0.25),0 3px 7px -3px rgba(0,0,0,0.3)',
+        }}
+        src={'/hero-video.mp4'}
+        width="1100"
+        height="800"
+        controls="controls"
+      />
+    </Box>
+  )
 }
 
 export default VideoDemo
