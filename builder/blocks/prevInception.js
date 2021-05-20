355a944d2c4a90a214f5d5995cd4b071a19bdd2d
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-05-20 10:18:45 +0200

    feat: added images backgrounds to sections

--- builder/blocks/prevInception.js
+++ builder/blocks/prevInception.js
@@ -3,25 +3,31 @@ import { RedirectWrapper } from './text'
 export const PrevInception = (props) => {
   const redirectUrl = props?.redirect
   const gradientColor = props.gradientColor
+
+  let background
+  if (gradientColor) {
+    background = `linear-gradient(225deg, ${gradientColor[0]} 0%, ${gradientColor[1]} 100%)`
+  } else if (props.backgroundColor && !props.imageUrl) {
+    background = props.background
+  }
   const inceptionModifiers = {
     boxShadow: props.boxShadow,
-    background: gradientColor
-      ? `linear-gradient(225deg, ${gradientColor[0]} 0%, ${gradientColor[1]} 100%)`
-      : props.backgroundColor,
+    ...(background ? { background } : {}),
     borderRadius: props.borderRadius,
     border: props.border,
   }
+  console.log(inceptionModifiers, gradientColor)
   return (
     <RedirectWrapper redirectUrl={redirectUrl}>
       <div
         style={{
           width: '100%',
           height: '100%',
+          margin: 'auto',
           backgroundImage: `url(${props.imageUrl})`,
           backgroundPosition: '50% 50%',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
-          margin: 'auto',
           ...inceptionModifiers,
         }}
       />
