d7fb5217984931f0aa4c79dded98858c3ad8ec72
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-05-20 10:21:34 +0200

    feat: smartsapp async loading

--- components/landing-page/index.js
+++ components/landing-page/index.js
@@ -13,7 +13,7 @@ import Footer from './footer'
 
 import { initLiveChatScript } from '../../utils/analytics'
 
-function loadScript() {
+async function loadScript() {
   window && window.eval(initLiveChatScript)
 }
 
