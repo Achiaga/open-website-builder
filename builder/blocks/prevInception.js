fa43cf52c0719a34362fb4ec5d7388ae969eb3de
Achiaga <alfonso.achiaga@gmail.com>
2021-05-25 10:19:23 +0200

    feat: opacity inception preview

--- builder/blocks/prevInception.js
+++ builder/blocks/prevInception.js
@@ -29,7 +29,21 @@ export const PrevInception = (props) => {
           backgroundSize: 'cover',
           ...inceptionModifiers,
         }}
-      />
+      >
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#000000',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            opacity: props.opacity || 0,
+            overflow: 'hidden',
+            borderRadius: props.borderRadius,
+          }}
+        />
+      </div>
     </RedirectWrapper>
   )
 }
