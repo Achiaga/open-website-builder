7e136521e2df24f5a8d42ca529a680af536fc89c
gonzalo10 <gonzalo.achiaga@gmail.com>
2021-05-26 07:51:00 +0200

    feat: changed code to the _app fiile

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,14 @@ import '../styles/globals.css'
 import '../builder/web-builder/styles.css'
 import '../styles/QuillStyle.css'
 import '../styles/tableStyles.css'
+import { NextSeo } from 'next-seo'
+
+const currentURL = 'https://www.antfolio.app'
+const previewImage = 'https://www.antfolio.app/social-image.png'
+const siteName = 'Antfolio'
+const pageTitle = 'Build Beautiful Websites Faster than ever!'
+const description =
+  'Antfolio is the fastest and easiest way to build beautiful websites with no-code. Have your blazing fast website live in minutes.'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -35,13 +43,41 @@ function MyApp({ Component, pageProps }) {
     }
   }, [router.events])
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={customTheme}>
-        <CSSReset />
-        <Fonts />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <>
+      <NextSeo
+        title={pageTitle}
+        description={description}
+        canonical={currentURL}
+        openGraph={{
+          type: 'website',
+          url: currentURL,
+          title: pageTitle,
+          description: description,
+          images: [
+            {
+              url: previewImage,
+              width: 1200,
+              height: 628,
+              alt: 'Antfolio social image',
+            },
+          ],
+          site_name: siteName,
+        }}
+        twitter={{
+          image: previewImage,
+          handle: '@antfolio_app',
+          site: siteName,
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Provider store={store}>
+        <ThemeProvider theme={customTheme}>
+          <CSSReset />
+          <Fonts />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </>
   )
 }
 MyApp.propTypes = {
