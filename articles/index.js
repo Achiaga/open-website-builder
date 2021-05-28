d305d137ba2136dc30da5b208f8b90e84333b7d3
Achiaga <alfonso.achiaga@gmail.com>
2021-05-28 11:18:19 +0200

    fix: error build

--- articles/index.js
+++ articles/index.js
@@ -2,6 +2,10 @@ import Article2 from './article-2'
 import GuideCustomDomain from '../templates/guides-custom-domain.json'
 import GuideIntegrationFlurly from '../templates/guides-integration-flurly.json'
 
+export default {
+  'top-100-website-builder-comparison': Article2,
+}
+
 export const guides = {
   'guides-custom-domain': GuideCustomDomain,
   'guides-integration-flurly': GuideIntegrationFlurly,
